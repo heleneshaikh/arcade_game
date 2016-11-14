912cbc1527836afe5bd417af1dfb6140f1f08590
heleneshaikh <helene.shaikh@gmail.com>
2016-11-14 14:41:23 +0100

    make play move left, right, up, down

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var Enemy = function(startX, startY, speed) {
+var Enemy = function (startX, startY, speed) {
     this.x = startX;
     this.y = startY;
     this.speed = speed;
@@ -6,50 +6,60 @@ var Enemy = function(startX, startY, speed) {
 };
 
 Enemy.prototype = {
-    update : function(dt) { // You should multiply any movement by the dt parameter which will ensure the game runs at the same speed for all computers.
-
+    update: function (dt) {
+        // this.x = this.x + this.speed * dt;
     },
-    render: function() {
+    render: function () {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 };
 
-var Player = function(startX, startY, speed) {
-    this.x = startX;
-    this.y = startY;
-    this.speed = speed;
+var initialX = 202;
+var initialY = 400;
+var Player = function () {
+    this.x = initialX;
+    this.y = initialY;
     this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype = {
-    update: function(dt) {
+    update: function (dt) {
 
     },
-    render: function() {
+    render: function () {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     },
-    handleInput : function() {
-
+    handleInput: function (keyPressed) {
+      switch (keyPressed) {
+          case 'left':
+              this.x = this.x - 100;
+              break;
+          case 'up':
+              this.y = this.y - 100;
+              break;
+          case 'right':
+              this.x = this.x + 100;
+              break;
+          case 'down':
+              this.y = this.y + 100;
+              break;
+      }
     }
-
 };
 
 var enemyOne = new Enemy(0, 65);
 var enemyTwo = new Enemy(0, 145);
 var enemyThree = new Enemy(0, 230);
 var allEnemies = [enemyOne, enemyTwo, enemyThree];
-var player = new Player(202, 400);
+var player = new Player();
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
-        37: 'left',
+        37: 'left', //key codes
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+    var pressedKey = allowedKeys[e.keyCode];
+    player.handleInput(pressedKey);
 });
